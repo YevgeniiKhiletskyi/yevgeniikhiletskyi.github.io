de22b717720c9f0610ed49dfcf70401d9a49a98c
Yevgenii Khiletskyi <yevgeniikhiletskyi@gmail.com>
2020-12-10 10:48:16 +0200

    редактирование кода

--- js/app.js
+++ js/app.js
@@ -5,75 +5,75 @@ let intervalId;
 
 
 $().ready(function(){
-    video = document.getElementById("player"); 
+  video = document.getElementById("player"); 
 
-    video.addEventListener('click', playStop);
+  video.addEventListener('click', playStop);
 
-    let playButtons = document.querySelectorAll(".play");
-    for (let i = 0; i < playButtons.length;i++){
-        playButtons[i].addEventListener('click',playStop);
-    }
+  let playButtons = document.querySelectorAll(".play");
+  for (let i = 0; i < playButtons.length;i++){
+    playButtons[i].addEventListener('click',playStop);
+  }
 
-    let micControl = document.getElementById("mic");
-    micControl.addEventListener('click',soundOf)
+  let micControl = document.getElementById("mic");
+  micControl.addEventListener('click',soundOf)
     
-    durationControl = document.getElementById("durationLevel");    
-    durationControl.addEventListener('click',setVideoDuration);
-    durationControl.addEventListener('onmousemove',setVideoDuration);
-    durationControl.addEventListener('mousedown', stopInterval); 
-    durationControl.min = 0;
-    durationControl.value = 0;    
+  durationControl = document.getElementById("durationLevel");    
+  durationControl.addEventListener('click',setVideoDuration);
+  durationControl.addEventListener('onmousemove',setVideoDuration);
+  durationControl.addEventListener('mousedown', stopInterval); 
+  durationControl.min = 0;
+  durationControl.value = 0;    
 
-    soundControl = document.getElementById("micLevel");    
-    soundControl.addEventListener('click', changeSoundVolume);
-    soundControl.addEventListener('onmousemove', changeSoundVolume);
+  soundControl = document.getElementById("micLevel");    
+  soundControl.addEventListener('click', changeSoundVolume);
+  soundControl.addEventListener('onmousemove', changeSoundVolume);
 
-    soundControl.min = 0;
-    soundControl.max = 10;
-    soundControl.value = soundControl.max;
+  soundControl.min = 0;
+  soundControl.max = 10;
+  soundControl.value = soundControl.max;
     
 });
 
 function playStop(){
-    $(".player-pic").toggleClass("player-pic_active");
+  $(".player-pic").toggleClass("player-pic_active");
     
-    durationControl.max = video.duration;
+  durationControl.max = video.duration;
 
-    if (video.paused){
-        video.play();
-        intervalId = setInterval(updateDuration,1)
-    }else{
-        video.pause();  
-        clearInterval(intervalId);
-    }
+  if (video.paused){
+    video.play();
+    intervalId = setInterval(updateDuration,1)
+  }else{
+    video.pause();  
+    clearInterval(intervalId);
+  }
 }
 
 function soundOf(){    
 
-    if (video.volume ===0){
-        video.volume = soundLevel;
-        soundControl.value = soundLevel*10;
-    }else{
-        soundLevel = video.volume;
-        video.volume = 0;
-        soundControl.value = 0;
-    }    
+  if (video.volume ===0){
+    video.volume = soundLevel;
+    soundControl.value = soundLevel*10;
+  }else{
+    soundLevel = video.volume;
+    video.volume = 0;
+    soundControl.value = 0;
+  }    
 }
 
 function stopInterval(){
-    clearInterval(intervalId);
+  clearInterval(intervalId);
 }
 
 function setVideoDuration(){
-    video.currentTime = durationControl.value;   
-    intervalId = setInterval(updateDuration,1000/66);    
+  video.currentTime = durationControl.value;   
+  intervalId = setInterval(updateDuration,1000/66);    
 }
 
 function changeSoundVolume(){
 
-    video.volume = soundControl.value/10;  
+  video.volume = soundControl.value/10;  
 }
 
 function updateDuration(){    
-    durationControl.value = video.currentTime;
+  durationControl.value = video.currentTime;
 }
